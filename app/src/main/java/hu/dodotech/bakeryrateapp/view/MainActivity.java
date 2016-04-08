57610487fd0807a2f174bfb113a891d12475e385
adorjanszeles <szeles.adorjan@hotmail.com>
2016-04-08 20:03:52 +0200

    Create the main structure of the application!

--- app/src/main/java/hu/dodotech/bakeryrateapp/view/MainActivity.java
+++ app/src/main/java/hu/dodotech/bakeryrateapp/view/MainActivity.java
@@ -1,7 +1,9 @@
-package hu.dodotech.bakeryrateapp;
+package hu.dodotech.bakeryrateapp.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import hu.dodotech.bakeryrateapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
