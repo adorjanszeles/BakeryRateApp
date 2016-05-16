e4c551aa18bc5dd0f3df1adefa7f9c5e0363ef64
adorjanszeles <szeles.adorjan@hotmail.com>
2016-05-16 13:54:49 +0200

    Enable multi dex to avoid Travis CI build failure!

--- app/src/mock/java/hu/dodotech/bakeryrateapp/BakeryApp.java
+++ app/src/mock/java/hu/dodotech/bakeryrateapp/BakeryApp.java
@@ -1,6 +1,8 @@
 package hu.dodotech.bakeryrateapp;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import hu.dodotech.bakeryrateapp.view.UIModule;
 
@@ -11,6 +13,12 @@ public class BakeryApp extends Application {
     public void onCreate() {
         super.onCreate();
         injector = DaggerMockBakeryAppComponent.builder().uIModule(new UIModule()).build();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 
     public void setInjector(BakeryAppComponent injector) {
